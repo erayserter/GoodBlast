325a983376b9f246fc56438b87ffed26c5c84217
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2024-02-02 18:36:28 +0300

    user group ranking router name changed.

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('global', GlobalLeaderboard.as_view(), name='global-leaderboard'),
     path('country', CountryLeaderboard.as_view(), name='country-leaderboard'),
     path('group', GroupLeaderboard.as_view(), name='group-leaderboard'),
-    path('rank', UserGroupRank.as_view(), name='user-rank'),
+    path('rank', UserGroupRank.as_view(), name='user-group-rank'),
 ]
